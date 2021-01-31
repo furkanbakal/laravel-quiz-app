64e23f3d6565cb80f5228a516eb011a7ddefd481
Furkan Bakal <f.bakal1997@gmail.com>
2021-01-31 23:37:00 +0300

    quiz migration and seed

---
+++ database/seeders/QuizSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class QuizSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        \App\Models\Quiz::factory(10)->create();
+    }
+}
